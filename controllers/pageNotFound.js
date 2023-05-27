faff178d34d9094a07914d459b029cc18c9ecfb2
Ivan Vahonin <vahoninivan1992@yandex.ru>
2023-05-27 20:05:55 +0300

    автотесты 4

--- controllers/pageNotFound.js
+++ controllers/pageNotFound.js
@@ -1,4 +1,5 @@
+const ERROR_CODE_NOTFOUND = 404;
 
 module.exports.pageNotFound = (req, res) => {
-  res.send({message: 'Страница не найдена'});
+  res.status(ERROR_CODE_NOTFOUND).send({message: 'Страница не найдена'});
 };
